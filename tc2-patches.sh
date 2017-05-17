9c04518853954e136fa6945e55464cc420f8300e
Alex <lausklaus44@gmail.com>
2017-05-17 16:49:19 +0200

    fix Google's stupid errors
    seriously, what's wrong with those code snippets on an android phone?

--- tc2-patches.sh
+++ tc2-patches.sh
@@ -2,27 +2,27 @@
 # my first bash script, testing to automate cruel 
 # work. You might need to run this as root.
 
-echo “downloading dependencies”
+echo "downloading dependencies"
 
 sudo apt-get install build-essential libdigest-sha-perl patchutils libproc-processtable-perl git-core bc ncurses-dev
 
-echo “downloading raspbian kernel headers”
+echo "downloading raspbian kernel headers"
 
 sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
 
-echo “and now executing it”
+echo "and now executing it"
 
 rpi-source
 
-echo “cloning V4L files next”
+echo "cloning V4L files next"
 
 git clone git://linuxtv.org/media_build.git
 
-echo “fetching custom firmware files”
+echo "fetching custom firmware files"
 
 git clone https://github.com/OpenELEC/dvb-firmware.git
 
-echo “and installing them to /lib/firmware/”
+echo "and installing them to /lib/firmware/"
 
 cd dvb-firmware
 
@@ -30,11 +30,11 @@ sudo ./install
 
 cd ..
 
-echo “Done. On to the hard part.”
+echo "Done. On to the hard part."
 
-echo “This takes up to an hour,”
+echo "This takes up to an hour,"
 
-echo “Go for a walk or make some tea.”
+echo "Go for a walk or make some tea."
 
 cd media_build/v4l/
 
@@ -46,7 +46,7 @@ cd ..
 
 ./build
 
-echo “½ done. Patching necessary files next”
+echo "50% done. Patching necessary files next"
 
 cd linux/drivers/media/usb/dvb-usb-v2/
 
@@ -70,14 +70,14 @@ cd
 
 cd media_build
 
-echo “Done patching files. Next step will take ~1h again”
+echo "Done patching files. Next step will take ~1h again"
 
 make allyesconfig
 
 make
 
-echo “Finalizing setup”
+echo "Finalizing setup"
 
 sudo make install
 
-echo “Setup finished, now plug in your TC2 stick and run dmesg”
+echo "Setup finished, now plug in your TC2 stick and run dmesg"
