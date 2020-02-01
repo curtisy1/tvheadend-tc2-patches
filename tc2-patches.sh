7a310d18afd24aa42b307cb8554bef2a04fc3e4f
curtisy <lausklaus44@gmail.com>
2020-02-01 13:30:07 +0100

    Update patches

--- tc2-patches.sh
+++ tc2-patches.sh
@@ -3,9 +3,6 @@
 git clone git://linuxtv.org/media_build.git
 cd media_build
 ./build
-patch linux/drivers/media/usb/dvb-usb-v2/af9035.c patchfiles/af9035.patch
-mv patchfiles/si2157* linux/drivers/media/tuners/
-cd linux/drivers/media/usb/dvb-usb-v2/
-make allyesconfig
+cp ../patchfiles/si2157* linux/drivers/media/tuners/
 make
 make install
