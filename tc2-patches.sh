89a5a4396fb96b439245b8305c830157f02f112b
curtisy <lausklaus44@gmail.com>
2020-02-29 16:43:44 +0100

    Copy over file

--- tc2-patches.sh
+++ tc2-patches.sh
@@ -4,6 +4,7 @@ git clone git://linuxtv.org/media_build.git
 cd media_build
 ./build
 cp ../patchfiles/si2157* linux/drivers/media/tuners/
+cp ../patchfiles/af9035.c linux/drivers/media/usb/dvb-usb-v2/af9035.c
 sudo make
 sudo make install
 sudo wget -c 'https://raw.githubusercontent.com/LibreELEC/dvb-firmware/master/firmware/dvb-demod-si2168-b40-01.fw' -o /lib/firmware/dvb-demod-si2168-b40-01.fw
